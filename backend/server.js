8c535119bafcf8d76e760a93fd34dd6e2e22cd58
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2025-02-27 07:43:15 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 5000;
 // app.use(morgan('dev'));
 app.use(express.json());
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use(cors());
+//app.use(cors());
+
+app.use(cors({ origin:https://transport-project-azure.vercel.app/}));
+
 
 app.use('/', allRoutes);
 
