65e5dc1a7339d89e97118ba86dea258266d47527
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2025-02-27 07:17:49 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 // app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+// app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors());
+
 app.use('/', allRoutes);
 
 
